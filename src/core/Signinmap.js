626eb960f381fc4b059741668f481a8fc7f82d2b
Chandru <chandru6501@gmail.com>
2022-03-09 03:33:47 +0530

    reloaddfsdjnvvhn

--- src/core/Signinmap.js
+++ src/core/Signinmap.js
@@ -37,7 +37,7 @@ const Signinmap = props => {
     console.log(coordinates)
     const clickSubmit = (e)=>{
         e.preventDefault();
-        console.log("firstnv vbvnbmbmbnbbjbnjb");
+        console.log("firstnv vbvnbmbmbnbbjbnjdvvdb");
     }
     useEffect(() => {
         location.reload();
